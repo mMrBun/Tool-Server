59ecd78e294bf32c70714fe3e77a0fee66a66299
siuMing <sunhanming@kitking.com>
2024-01-04 17:37:25 +0800

    update annotation

--- api/auth/auth_controller.py
+++ api/auth/auth_controller.py
@@ -2,16 +2,16 @@ from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
-from db.curd.sys_user_dao import find_sys_user_by_username
+from db.curd.patients_dao import query_patients_by_username
 from db.database import get_db
-from utils import TokenResponse, BaseResponse, create_token
+from utils import BaseResponse, create_token
 from utils.token import verify_token
 
 
 def login(
         form_data: OAuth2PasswordRequestForm = Depends(),
         db: Session = Depends(get_db)
-) -> TokenResponse:
+) -> BaseResponse:
     """
     User login
     :param form_data: OAuth2PasswordRequestForm Object
@@ -20,15 +20,15 @@ def login(
     """
     username = form_data.username
     password = form_data.password
-    sys_user = find_sys_user_by_username(db, username)
+    patient = query_patients_by_username(db, username)
 
-    if sys_user is None or sys_user.password != password:
+    if patient is None or patient.password != password:
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
     # create token
     token = create_token({"sub": username})
-    response = TokenResponse()
-    response.access_token = token
+    response = BaseResponse()
+    response.data = {"access_token": token, "token_type": "bearer"}
     return response
 
 
@@ -37,7 +37,7 @@ def hello(current_user: dict = Depends(verify_token)):
     Test token Authentication
     """
 
-    sys_user = current_user.get("sys_user")
+    patients = current_user.get("patients")
 
-    print(sys_user.username, sys_user.password)
-    return BaseResponse(code=200, msg="success", data="hello")
+    print(patients.username, patients.password)
+    return BaseResponse(code=200, msg="success", data=patients)
