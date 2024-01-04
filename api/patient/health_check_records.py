97a3aa5c9e8ce124ce2e649c32cf8896e6f1908a
mMrBun <xuhuik@digitalchina.com>
2024-01-04 18:30:59 +0800

    ...

--- api/patient/health_check_records.py
+++ api/patient/health_check_records.py
@@ -1,6 +1,16 @@
+from fastapi import Depends
+from requests import Session
+
+from db.database import get_db
+from utils.token import verify_token
+from db.models.health_check_records import HealthCheckRecords
+from db.models.health_check_results import HealthCheckResults
 
 
 def get_health_check_records(
-
+        current_user: dict = Depends(verify_token),
+        db: Session = Depends(get_db)
 ):
-    pass
+    ...
+    print(current_user.values())
+    print(db)
