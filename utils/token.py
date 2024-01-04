87d688440e372923b2961f165ccc98a451a109d0
mMrBun <2015711377@qq.com>
2024-01-04 22:31:47 +0800

    change error response

--- utils/token.py
+++ utils/token.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime, timedelta
 from typing import Union
 import jwt
@@ -8,6 +9,8 @@ from sqlalchemy.orm import Session
 
 from db.curd.patients_dao import query_patients_by_username
 import db.database
+from utils import BaseResponse
+from utils.error_code import ErrorCode
 
 SECRET_KEY = "tool-server"
 ALGORITHM = "HS256"
@@ -48,24 +51,44 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/login")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-def verify_token(token: str = Depends(oauth2_scheme), db: Session = Depends(db.database.get_db)):
-    credentials_exception = HTTPException(
-        status_code=401,
-        detail="Invalid credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-
+def verify_token(token: str = Depends(oauth2_scheme), db: Session = Depends(db.database.get_db)) -> dict:
     try:
         payload = decode_token(token)
+        if not payload:
+            logging.error(ErrorCode.TOKEN_INVALID_ERROR.msg)
+            return BaseResponse(
+                code=ErrorCode.TOKEN_INVALID_ERROR.code,
+                msg=ErrorCode.TOKEN_INVALID_ERROR.msg,
+                data=[]
+            ).dict()
+
         username: str = payload.get("sub")
         if username is None:
-            raise credentials_exception
-        sys_user = query_patients_by_username(db, username)
-        if sys_user is None:
-            raise credentials_exception
-        payload.update({"sys_user": sys_user})
-    except EOFError:
-        raise credentials_exception
+            logging.error(ErrorCode.USERNAME_OR_PASSWORD_ERROR.msg)
+            return BaseResponse(
+                code=ErrorCode.USERNAME_OR_PASSWORD_ERROR.code,
+                msg=ErrorCode.USERNAME_OR_PASSWORD_ERROR.msg,
+                data=[]
+            ).dict()
+
+        patient = query_patients_by_username(db, username)
+        if patient is None:
+            logging.error(ErrorCode.USER_NOT_FOUND_ERROR.msg)
+            return BaseResponse(
+                code=ErrorCode.USER_NOT_FOUND_ERROR.code,
+                msg=ErrorCode.USER_NOT_FOUND_ERROR.msg,
+                data=[]
+            ).dict()
+
+        payload.update({"patient": patient, "code": 200})
+    except Exception as e:
+        logging.error(ErrorCode.UNKNOWN_ERROR.msg + e)
+        return BaseResponse(
+            code=ErrorCode.UNKNOWN_ERROR.code,
+            msg=ErrorCode.UNKNOWN_ERROR.msg + e,
+            data=[]
+        ).dict()
+
     return payload
 
 
@@ -77,4 +100,3 @@ def verify_password(plain_password, hashed_password):
 
 def hash_password(plain_password):
     return pwd_context.hash(plain_password)
-
