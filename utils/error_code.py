6a82fd7325c19e0883a9aa2af1e6f5d3902ce991
mMrBun <2015711377@qq.com>
2024-01-04 22:32:37 +0800

    add error code & msg😅

--- utils/error_code.py
+++ utils/error_code.py
@@ -3,6 +3,10 @@ from enum import Enum
 
 class ErrorCode(Enum):
     INSERT_ERROR = (100001, "新增数据出错，请查看日志ERROR")
+    TOKEN_INVALID_ERROR = (401, "Token失效，请重新登录")
+    USERNAME_OR_PASSWORD_ERROR = (100002, "用户名或密码错误")
+    USER_NOT_FOUND_ERROR = (100003, "用户不存在，请检查账号或密码")
+    UNKNOWN_ERROR = (100004, "未知异常：")
 
     def __init__(self, code, msg):
         self.code = code
